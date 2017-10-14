65eacea49fb023bd2e8e95a28eb9e0189ef537c6
Gigihnovika <32793187+Gigihnovika@users.noreply.github.com>
2017-10-14 19:01:11 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,12 @@
+<html>
+<body bgcolor="cyan">
+<p align="center"><font size="7px">Register Anda Berhasil</font></p>
+<p align ="center"><table bgcolor="magenta">
+<tr><font size="3px"><td>Nama Anda</td> <td>:</td><td><?php echo $_POST["full-name"];?></td></font></tr>
+<tr><font size="3px"><td>Username Anda</td> <td>:</td><td><?php echo $_POST["username"];?></td></font></tr>
+<tr><font size="3px"><td>Password Anda</td> <td>:</td><td><?php echo $_POST["password"];?></td></font></tr>
+</table>
+</p>
+<p align="center"><font size="5px">Selamat Bergabung di Instagram</font></p>
+</body>
+</html>
